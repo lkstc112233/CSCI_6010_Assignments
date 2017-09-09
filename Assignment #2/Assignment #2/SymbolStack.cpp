92f1f5b1a9fac9e0dde5705e45032c3944f87828
Photon Cat <lkstc112233@gmail.com>
2017-09-09 13:32:31 -0400

    2. That's a stack for Symbol

--- Assignment #2/Assignment #2/SymbolStack.cpp
+++ Assignment #2/Assignment #2/SymbolStack.cpp
@@ -6,3 +6,48 @@
 //
 
 #include "SymbolStack.hpp"
+
+namespace Assignment2 {
+    CStackForSymbol::CStackForSymbol()
+    {
+        max_size = 20;
+        array = new CSymbol[max_size];
+        m_size = 0;
+    }
+    
+    CStackForSymbol::~CStackForSymbol()
+    {
+        delete[] array;
+    }
+    
+    void CStackForSymbol::extend()
+    {
+        auto newarray = new CSymbol[max_size * 2];
+        for (int i = 0; i < max_size; ++i)
+            newarray[i] = array[i];
+        delete[] array;
+        array = newarray;
+        max_size *= 2;
+    }
+    
+    void CStackForSymbol::push(const CSymbol &toPush)
+    {
+        if (m_size >= max_size)
+            extend();
+        array[m_size++] = toPush;
+    }
+    
+    CSymbol& CStackForSymbol::top()
+    {
+        if (m_size <= 0) // Prevent this from happening!
+            throw CSymbol();
+        return array[m_size - 1];
+    }
+    
+    CSymbol CStackForSymbol::pop()
+    {
+        auto result = top();
+        --m_size;
+        return result;
+    }
+}
--- Assignment #2/Assignment #2/SymbolStack.hpp
+++ Assignment #2/Assignment #2/SymbolStack.hpp
@@ -9,5 +9,30 @@
 #define SymbolStack_hpp
 
 #include <stdio.h>
+#include "Symbol.hpp"
+
+/* This stack could be a template.
+ * But in this assignment, the only
+ * useage of the stack would be the
+ * Symbol stack, so...
+ */
+
+namespace Assignment2 {
+    class CStackForSymbol
+    {
+    public:
+        CStackForSymbol();
+        ~CStackForSymbol();
+        CSymbol& top();
+        CSymbol pop();
+        void push(const CSymbol&);
+        bool empty();
+    private:
+        CSymbol* array;
+        int max_size;
+        int m_size;
+        void extend();
+    };
+}
 
 #endif /* SymbolStack_hpp */
