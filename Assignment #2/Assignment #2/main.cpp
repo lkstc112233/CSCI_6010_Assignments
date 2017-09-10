75d125cbe0843d5677b25ab8480df0ced4ef6f44
Photon Cat <lkstc112233@gmail.com>
2017-09-10 17:15:42 -0400

    2. Interface Improvements.

--- Assignment #2/Assignment #2/main.cpp
+++ Assignment #2/Assignment #2/main.cpp
@@ -6,6 +6,7 @@
 //
 
 #include <iostream>
+#include <fstream>
 #include <deque>
 #include "ExpressionMachine.hpp"
 #include "LexicalAnalyser.hpp"
@@ -82,10 +83,75 @@ void machineTest()
     }
 }
 
+
 int main(int argc, const char * argv[]) {
-    // insert code here...
+    std::ifstream file;
+    bool fromFile = false;
+    if (argc > 1)
+    {
+        fromFile = true;
+        file.open(argv[1]);
+    }
     
-    machineTest();
+    Assignment2::CLexicalAnalyser engine(fromFile? file: std::cin);
+    Assignment2::CExpressionMachine machine;
     
+    std::deque<Assignment2::CSymbol> inputs;
+    
+    if (!fromFile)
+        cout << ">> ";
+    while (true)
+    {
+        try
+        {
+            auto c = engine.getNextToken();
+            if (c.getType() == Assignment2::OPERATOR
+                && c.getAdditionalInformation().m_operator == Assignment2::NEWLINE)
+            {
+                if (fromFile)
+                {
+                    cout << "Input: ";
+                    for (auto c : inputs)
+                        cout << c;
+                    cout << endl;
+                }
+                machine.compile(inputs);
+                cout << "RPN: " << machine.getCompiledExpression() << endl;
+                cout << "Result: " << machine.evaluateExpression() << endl;
+                inputs.clear();
+                if (!fromFile)
+                    cout << ">> ";
+            }
+            else
+                inputs.push_back(c);
+        }
+        catch(Assignment2::NeedToExitException)
+        {
+            if (!fromFile)
+                cout << "Thank you for using RPN calculator." << endl;
+            break;
+        }
+        catch(Assignment2::SyntaxErrorException)
+        {
+            std::cerr << "Syntax error." << endl;
+            inputs.clear();
+            if (!fromFile)
+                cout << ">> ";
+        }
+        catch(Assignment2::MathErrorException)
+        {
+            std::cerr << "Math error." << endl;
+            inputs.clear();
+            if (!fromFile)
+                cout << ">> ";
+        }
+        catch(Assignment2::UnexpectedOperatorException)
+        {
+            std::cerr << "Unexpected Operation" << endl;
+            inputs.clear();
+            if (!fromFile)
+                cout << ">> ";
+        }
+    }
     return 0;
 }
