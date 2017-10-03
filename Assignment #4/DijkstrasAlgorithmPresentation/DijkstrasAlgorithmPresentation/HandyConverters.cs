374e90e9d3a4110bbdaaeaeb114a993062dd23de
Quantum cat <lkstc112233@users.noreply.github.com>
2017-10-03 15:59:03 -0400

    4. Slight Changes

--- Assignment #4/DijkstrasAlgorithmPresentation/DijkstrasAlgorithmPresentation/HandyConverters.cs
+++ Assignment #4/DijkstrasAlgorithmPresentation/DijkstrasAlgorithmPresentation/HandyConverters.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Controls;
 using System.Windows.Data;
 
 namespace DijkstrasAlgorithmPresentation
@@ -23,4 +24,34 @@ namespace DijkstrasAlgorithmPresentation
             throw new NotImplementedException();
         }
     }
+
+    class VertexToContentConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return Binding.DoNothing;
+        }
+    }
+
+    class UIElementToXConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value == null)
+                return Binding.DoNothing;
+            UIElement elem = value as UIElement;
+            return (Canvas.GetLeft(elem) + Canvas.GetRight(elem)) / 2;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return Binding.DoNothing;
+        }
+    }
 }
+
